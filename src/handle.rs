fc69c529e2769134f46f80291229327b837ae762
amith_athreya <athreya.amith88@gmail.com>
2024-12-28 14:10:26 +0530

    add 404 NF error

--- src/handle.rs
+++ src/handle.rs
@@ -5,25 +5,34 @@ use std::fs;
 
 pub fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_req: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let content = fs::read_to_string("index.html").unwrap_or_else(|_| {
+            "Error: Could not read the file.".to_string()
+        });
+        let length = content.len();
 
-    println!("Received request: {:?}", http_req);
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{content}"
+        );
 
-    let status_line = "HTTP/1.1 200 OK";
-    let content = fs::read_to_string("index.html").unwrap_or_else(|_| {
-        "Error: Could not read the file.".to_string()
-    });
-    let length = content.len();
+        if let Err(e) = stream.write_all(response.as_bytes()) {
+            eprintln!("Failed to send response: {}", e);
+        }
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let content = fs::read_to_string("404.html").unwrap_or_else(|_| {
+            "Error: Could not read the file.".to_string()
+        });
+        let length = content.len();
 
-    let response = format!(
-        "{status_line}\r\nContent-Length: {length}\r\n\r\n{content}"
-    );
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{content}"
+        );
 
-    if let Err(e) = stream.write_all(response.as_bytes()) {
-        eprintln!("Failed to send response: {}", e);
+        if let Err(e) = stream.write_all(response.as_bytes()) {
+            eprintln!("Failed to send response: {}", e);
+        }
     }
 }
